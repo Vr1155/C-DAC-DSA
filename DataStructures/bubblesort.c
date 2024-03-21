a4eb5d4d1d6817fd8214f72d379cb200d8e8c9ea
Vr1155 <49389720+Vr1155@users.noreply.github.com>
2024-03-21 14:54:59 +0530

    Optimizing bubble sort

--- DataStructures/bubblesort.c
+++ DataStructures/bubblesort.c
@@ -29,15 +29,20 @@ void main(){
 
 void bubbleSort(int *arr, int size){
 	printf("Inside bubble sort function...\n");
-	int temp;
+	int temp, swapped = 0;
 	for(int i=0; i<size-1; i++){
-		for(int j=0; j<size-1; j++){
+		for(int j=0; j<size-i-1; j++){
 			if(arr[j]>arr[j+1])
 			{
 				temp = arr[j];
 				arr[j] = arr[j+1];
 				arr[j+1] = temp;
+				swapped = 1;
 			}	
+		}
+
+		if(swapped == 0){
+			return;
 		}
 	}
 	printf("Exiting bubble sort function!\n");
